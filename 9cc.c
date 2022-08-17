564fff238a75b34f03fad7eb80a56af43dcd742e
mixmaru <mix.touch777@gmail.com>
2022-08-17 16:05:38 +0900

    単項演算子機能追加

--- 9cc.c
+++ 9cc.c
@@ -167,6 +167,7 @@ Node *new_num(int val) {
 Node *expr();
 Node *mul();
 Node *primary();
+Node *unary();
 
 // expr = mul ("+" mul | "-" mul)*
 Node *expr() {
@@ -182,18 +183,26 @@ Node *expr() {
     }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_binary(ND_MUL, node, primary());
+            node = new_binary(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_binary(ND_DIV, node, primary());
+            node = new_binary(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+Node *unary() {
+    if (consume('+'))
+        return primary();
+    if (consume('-'))
+        return new_binary(ND_SUB, new_num(0), primary());
+    return primary();
 }
 
 // primary = "(" expr ")" | num
